f7b1dfe0cec7103a166363abc6ec80720c8c5bb5
FireBlaze236 <saminps3@gmail.com>
2021-11-07 08:16:56 +0600

    Backend test

--- source/routes/timetableRoute.ts
+++ source/routes/timetableRoute.ts
@@ -7,4 +7,5 @@ router.get('/get/:id', timetableController.getTimetableData);
 router.get('/getstatic/:id', timetableController.getStaticTimetableData);
 router.post('/save', timetableController.saveTimetableDataDynamic);
 router.post('/savestatic', timetableController.saveTimetableDataStatic);
+
 export = router;
--- source/server.ts
+++ source/server.ts
@@ -51,9 +51,12 @@ router.use(bodyparser.json());
 
 router.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
     if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
         res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST PUT');
         return res.status(200).json({});
     }
